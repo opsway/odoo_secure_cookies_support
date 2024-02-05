c963a171fa10e304ed0da94edf8aa2b72e57c91f
Ed Chu <edchu@opsway.com>
2024-02-05 13:59:16 +0200

    Add bank details

---
+++ addons/invoice_report_bank_information/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Part of Probuse Consulting Service Pvt Ltd. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Invoice Report Bank Information',
+    'price': 19.0,
+    'summary': 'Customer Invoice Report with Company Bank Information',
+    'version':'3.1.2',
+    'currency': 'EUR',
+    'license': 'Other proprietary',
+    'support': 'contact@probuse.com',
+    'live_test_url': 'https://probuseappdemo.com/probuse_apps/invoice_report_bank_information/171',#'https://youtu.be/PLuEplSA1RA',
+    'category':'Accounting/Accounting',
+    'description': """
+            """,
+    'author' : 'Probuse Consulting Service Pvt. Ltd.',
+    'website': 'www.probuse.com',
+    'depends': ['account'],
+    'images': ['static/description/img.jpg'],
+    'data':[
+                'views/invoice_bank_report_view.xml',   
+    ],
+    'installable': True,
+    'application': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
