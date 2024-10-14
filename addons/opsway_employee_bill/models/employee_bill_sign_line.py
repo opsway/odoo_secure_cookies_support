94c661bfb232fad4807540c6498a4bf0b2bca025
Oleksandr Borysov <125874475+BorysovO@users.noreply.github.com>
2024-10-14 15:37:02 +0300

    Revert "[INT-439][ADD] fix"

--- addons/opsway_employee_bill/models/employee_bill_sign_line.py
+++ addons/opsway_employee_bill/models/employee_bill_sign_line.py
@@ -7,21 +7,13 @@ class EmployeeBillSignLine(models.Model):
 
     move_id = fields.Many2one('account.move', string='Move')
 
-    description = fields.Char()
+    description = fields.Char(related='move_id.employee_product_tag_id.name')
     period = fields.Char(readonly=True)
     quantity = fields.Float(compute='_compute_quantity', store=True)
     currency_field = fields.Many2one('res.currency', default=lambda self: self.env.company.currency_id)
     price = fields.Monetary(currency_field='currency_field')
     amount = fields.Monetary(string='Amount', currency_field='currency_field')
 
-    def _get_report_description(self):
-        self.ensure_one()
-        report_description = ''
-        if self.move_id.employee_product_tag_id:
-            report_description = self.move_id.employee_product_tag_id.name
-        elif self.move_id.employee_bill_first_line_id:
-            report_description = self.move_id.employee_bill_first_line_id.product_id.name or ''
-        return report_description
 
     @api.depends('price', 'amount')
     def _compute_quantity(self):
