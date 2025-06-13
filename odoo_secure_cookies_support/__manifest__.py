67872230b59cfe4fbb8a054982b3de9d76a24895
Artem Shelest <arshe@opsway.com>
2025-06-13 18:44:17 +0100

    [INT-448][FIX] manifest, add description

--- odoo_secure_cookies_support/__manifest__.py
+++ odoo_secure_cookies_support/__manifest__.py
@@ -1,12 +1,12 @@
 {
     'name': "Secure Cookies Support",
-    'version': "17.0.0.1.0",
+    'version': "17.0.1.0.0",
     'summary': "Secure Cookies Support",
-    'description': "Secure Cookies Support",
-    'author': "Lumirang",
-    'website': "https://lumirang.com",
+    'description': "Adds the Secure flag to every session and CSRF cookie served by Odoo",
+    'author': "OpsWay",
+    'website': "https://opsway.com",
     'category': "Quality",
-    'license': "Other proprietary",
+    'license': "LGPL-3",
     'depends': ['web'],
     'data': [
         'data/ir_config_parameter.xml',
@@ -23,5 +23,4 @@
     'installable': True,
     'application': True,
     'auto_install': False,
-    'sequence': 5,
 }
