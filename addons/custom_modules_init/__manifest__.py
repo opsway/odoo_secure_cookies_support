ee7646ad19dc6329ea0df916d7bc71ee82203fe4
chupryna <s.v.chupryna@gmail.com>
2025-07-16 14:19:52 +0300

    [INT-580][FIX] correct dependencies

--- addons/custom_modules_init/__manifest__.py
+++ addons/custom_modules_init/__manifest__.py
@@ -17,7 +17,7 @@
         'currency_name_res_partner_bank',
         'sh_email_enhancement',
         'sh_email_enhancement_extension',
-        'web_one2many_duplicate_cr',
+        'one2many_duplicate_lines',
         'account_reports_exclude_analytics',
         'odoo_secure_cookies_support',
     ],
